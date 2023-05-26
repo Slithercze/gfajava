480ecea2c07c0c3af7ad2d2687fab6dab40f6951
ales <ales@vdev.local>
2023-05-26 09:59:47 +0200

    extract interface TodoService

--- CA/barebonesSpringTodoH2/src/main/java/com/gfa/todosqlca/TodoService.java
+++ CA/barebonesSpringTodoH2/src/main/java/com/gfa/todosqlca/TodoService.java
@@ -1,58 +1,27 @@
 package com.gfa.todosqlca;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-@Service
-public class TodoService {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Todo> getAll() {
-        return entityManager
-                .createNativeQuery("select * from todo", Todo.class)
-                .getResultList();
-    }
+public interface TodoService {
+    List<Todo> getAll();
 
     // for science!
     @Transactional
-    public Todo create(String title) {
-        return create(title, false);
-    }
+    Todo create(String title);
 
     @Transactional
-    public Todo create(String title, boolean isUrgent) {
-        Todo t = new Todo();
-        t.setTitle(title);
-        t.setIsUrgent(isUrgent);
-        t.setIsDone(false);
+    Todo create(String title, boolean isUrgent);
 
-        entityManager.persist(t);
-        return t;
-    }
-
-    public Todo findByID(Long id) {
-        return entityManager.find(Todo.class, id);
-    }
+    Todo findByID(Long id);
 
     @Transactional
-    public Todo update(Todo t) {
-        entityManager.merge(t);
-        return t;
-    }
+    Todo update(Todo t);
 
     @Transactional
-    public Todo save(Todo t) {
-        entityManager.persist(t);
-        return t;
-    }
+    Todo save(Todo t);
 
     @Transactional
-    public void remove(Todo t) {
-        entityManager.remove(t);
-    }
+    void remove(Todo t);
 }
---
+++ CA/barebonesSpringTodoH2/src/main/java/com/gfa/todosqlca/TodoServiceEntityImpl.java
@@ -0,0 +1,64 @@
+package com.gfa.todosqlca;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TodoServiceEntityImpl implements TodoService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Todo> getAll() {
+        return entityManager
+                .createNativeQuery("select * from todo", Todo.class)
+                .getResultList();
+    }
+
+    // for science!
+    @Override
+    @Transactional
+    public Todo create(String title) {
+        return create(title, false);
+    }
+
+    @Override
+    @Transactional
+    public Todo create(String title, boolean isUrgent) {
+        Todo t = new Todo();
+        t.setTitle(title);
+        t.setIsUrgent(isUrgent);
+        t.setIsDone(false);
+
+        entityManager.persist(t);
+        return t;
+    }
+
+    @Override
+    public Todo findByID(Long id) {
+        return entityManager.find(Todo.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Todo update(Todo t) {
+        entityManager.merge(t);
+        return t;
+    }
+
+    @Override
+    @Transactional
+    public Todo save(Todo t) {
+        entityManager.persist(t);
+        return t;
+    }
+
+    @Override
+    @Transactional
+    public void remove(Todo t) {
+        entityManager.remove(t);
+    }
+}
